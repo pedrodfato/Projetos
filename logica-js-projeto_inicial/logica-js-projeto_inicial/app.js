6a84eed596026fcca7fc25f7f9b0cff16ab6c5fc
Pedro de Castro <pedro.casstrof@gmail.com>
2024-05-17 02:10:36 -0300

    jogo interativo

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,24 +1,38 @@
-alert('Adivinhe o número!');
-let mult = parseInt(Math.random()*100 + 1);
+let parabens_tentativas = document.querySelector("#parabens");
+let buton = document.querySelector("#button");
+let mult = parseInt(Math.random() * 100 + 1);
+let nmax = document.querySelector("#nmaxhtml");
+nmax.innerHTML = mult;
 let nmra = parseInt(Math.random() * mult);
-console.log(nmra);
+console.log(`Número aleatório: ${nmra}`);
 let tentativas = 1;
 let chute;
 
-while(chute != nmra){
-    chute = prompt(`Chute um número entre 0 e ${mult}`);
-    if(chute==nmra){
-        break;
-    }
-    else if (chute > nmra){
-        alert(`O número é menor que ${chute}`);
-    }
-    else{
-        alert(`O número é maior que ${chute}`);
-    }
-    tentativas ++;
-    console.log(tentativas);
-}
+const btn = document.querySelector("#send");
 
-let acerto = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Parabéns, você acertou com ${tentativas} ${acerto}!`);
+btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const numero = document.querySelector("#numero");
+
+
+    let value = numero.value;
+    console.log(value);
+    if (value < nmra) {
+        parabens_tentativas.innerHTML = `O número é maior que ${value}`;
+        tentativas++;
+    }
+    else if (value > nmra) {
+        parabens_tentativas.innerHTML = `O número é menor que ${value}`;
+        tentativas++;
+    }
+    else {
+        let acerto = tentativas > 1 ? 'tentativas' : 'tentativa';
+        parabens_tentativas.innerHTML = `Parabéns, você acertou com ${tentativas} ${acerto}`;
+        buton.style.visibility = 'visible';
+    }
+});
+
+
+
+
+
